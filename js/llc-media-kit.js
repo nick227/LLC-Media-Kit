cd72f350512305b742f9b8d67a2183f0f68dfdd4
Nicholas Rios <nicholas.jay.rios@gmail.com>
2012-01-26 08:26:22 -0600

    still merging - thursday morning

--- js/llc-media-kit.js
+++ js/llc-media-kit.js
@@ -34,12 +34,28 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 				$('div#container').html(res);
 				document.title = mediaKit.site.siteTitle;
 				
-				mediaKit.setupVirtualIpad(); // do we need to provision for non-inventory pages???
+	$('body, section, div').bind('mousedown.welcome', function() {
+	$('#inventory-stage .welcome-message').animate({
+    opacity: 0.25,
+    height: '0'
+  }, {queue:false, duration:600, easing: 'easeInExpo'}, function() {
+	$('body, section, div').unbind('mousedown.welcome');
+	$('#inventory-stage .welcome-message').remove();
+  });  
+  
+	
+	});
+	
 				mediaKit.setupArrowSubNav();
+				mediaKit.setupVirtualIpad(); // do we need to provision for non-inventory pages??? //divergent paths and a neat-o controller function
+				
+				mediaKit.initPresentationArrows('firstInit');//*** ADDED BY MIKE (1/25/12) - presentation buttons call ***//
 				globalNavPointer = $('.header-edge-pointer');
 				console.log(globalNavPointer);
 				mediaKit.setPointerTargetX('#' + startPage + '_mainNav');
-				$(window).resize(mediaKit.setPointerX);
+				
+				
+				$(window).resize(function(){mediaKit.setPointerX(); mediaKit.initPresentationArrows();}); //*** ADDED BY MIKE (1/25/12) replaced this resize function ***//
 				}
 				});
 		
@@ -81,18 +97,67 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 			globalNavPointer[0].style.left = pointerTargetX + 'px';
 		}
 	},
+	shiftIpadScreen: function(whereTo){ 					//*** ADDED BY MIKE (1/25/12)***//
+		var target = 0;
+		switch (whereTo){
+			case "player":
+			break;
+			case "home":
+			target = 510;
+			break;
+		}
+		$('#homeScreen').animate({left:(target-510)}, 300, function(){});
+	},
+	initPresentationArrows: function(e){					//*** ADDED BY MIKE (1/25/12)***//
+		var prevArrowX = $('#ipad').offset().left - $('#presentationPrev').width() - 80;
+		var nextArrowX = $('#ipad').offset().left + $('#ipad').width() + $('#presentationPrev').width() + 80;
+		var leftArrow = $('#presentationPrev');
+		var rightArrow = $('#presentationNext');
+		leftArrow.css({'left': prevArrowX});
+		rightArrow.css({'left': nextArrowX});
+		if(e=='firstInit'){
+			leftArrow.css({'opacity': 0});
+			rightArrow.css({'opacity': 0});
+			setTimeout(function(){		
+			leftArrow.animate({'opacity': 0.7}, 1000, function(){});
+			rightArrow.animate({'opacity': 0.7}, 1000, function(){});
+			}, 2000);
+			
+			leftArrow.click(function(){
+				var prevSubSelection = $('ul li.selected').prev();
+				if(prevSubSelection != undefined){
+					prevSubSelection.trigger('click');
+				}
+			});
+			rightArrow.click(function(){
+				var nextSubSelection = $('ul li.selected').next();
+				if(nextSubSelection != undefined){
+					nextSubSelection.trigger('click');
+				}
+			});
+		}		
+	},
 	setupVirtualIpad: function(){ //sets up interactive ipad on inventory screen
 		// fade screen images
-		$("#ipadScreen .screen").fadeTo(0,0.5);
+		//$("#ipadScreen .screen").fadeTo(0,0.5);
+		
+		window.zoomAni = setInterval(function() {
+			$("#ipadScreen a .zoom").each(function(i){
+				$(this).delay(i*200).fadeTo(300, 0.5, function(){
+					$(this).delay(100).fadeTo(300, 1);
+				});
+			});
+		}, 3000);
 		
 		// set bg, fade ads, ad events
 		$("#ipadScreen a .img").each(function() {
 			
 			var par = $(this).parent(),
-				src = par.siblings(".screen").attr('src');
+				screen = par.siblings(".screen"),
+				src = screen.attr('src');
 			
 			// set bg image				
-			$(this).css('background-image','url('+src+')').fadeTo(0,0);
+			$(this).css('background-image','url('+src+')');
 			
 			// Click ad event
 			$(this).add(this.nextSibling).click(function(event){
@@ -100,26 +165,37 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 					var c = par.attr('class').replace('ad_','').replace('zoom ','');
 					$('#'+c).trigger('click');
 					
-		var arrow = $('nav.nav-sub').find('img.pointerArrowSml');
-		var p = $('nav.nav-sub ul li').find('li.selected').position();
-		$(arrow).stop(true, true).animate({top:p.top}, 200, function(){});
-
-		
-		return false;
+					var arrow = $('nav.nav-sub').find('img.pointerArrowSml');
+					var p = $('nav.nav-sub ul li').find('li.selected').position();
+					$(arrow).stop(true, true).animate({top:p.top}, 200, function(){});
+					//return false;
 
 				}				
 			});
 			
+			// parent rollover effect
+			$(this).parent().hover(function(){
+				// hover in
+				screen.clearQueue().fadeTo(300, 0.5);
+				$(this).siblings('a').clearQueue().fadeTo(300,0);
+				$(this).find('.zoom').fadeTo(0,0);
+			},function(){
+				// hover out
+				screen.clearQueue().fadeTo(300, 1);
+				$(this).siblings('a').clearQueue().fadeTo(300,1);
+				$(this).find('.zoom').fadeTo(0,1);
+			});
+			
 		});
 		
-		$("a.zoom").fancybox({
+		$("a.fancybox").fancybox({
 			'titlePosition'	: 'inside',
 			'opacity'		: true,
-			'overlayShow'	: false,
+			'overlayShow'	: true,
 			'transitionIn'	: 'elastic',
-			'type'          : 'image',
-			'transitionOut'	: 'none'
+			'transitionOut'	: 'elastic',
 		});
+		
 		
 		// setup draggables 	
 		$('.draggable').draggable({ axis: 'y', snapMode: 'both' }).bind( "dragstop", function(event, ui) {
@@ -148,7 +224,7 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 		  }
 		  
 		  // Keep child drag from triggering parrent
-		  event.stopPropagation();
+		  //event.stopPropagation();
 		  		  
 		}); 
 				
@@ -170,9 +246,9 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 				$("#ipadScreen .draggable").animate({top:y2}, 300);
 				
 				// hide ads
-				$('#ipadScreen a').each(function(){
-					$(this).removeClass('active').find('.img').fadeTo(0, 0)
-				});
+//				$('#ipadScreen a').each(function(){
+//					$(this).removeClass('active').find('.img').fadeTo(0, 0)
+//				});
 				
 				// show triggered ad
 				ad.addClass('active').find('.img').fadeTo(0,1);
@@ -190,19 +266,22 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 			});
 			
 		})
-		.eq(0).trigger('click'); // first link make active on load
+		//.eq(0).trigger('click'); // first link make active on load
 		
 	},
 	setupArrowSubNav: function(){
-	$('nav.nav-sub').prepend('<img class="pointerArrowSml" src="images/blueSelArrow.png" style="position:absolute; z-index:2" />');
+	var dircheck = $('nav.nav-sub').attr('rel');
+	if(dircheck=='vertical'){
+	
+	$('nav.nav-sub').prepend('<img class="pointerArrowSml" src="images/blueSelArrow.png" style="position:absolute; z-index:2; display:none;" />');
 	function hoverItem(){
+	$('nav.nav-sub').find('img.pointerArrowSml').fadeIn()
 		var arrow = $(this).parents('nav.nav-sub').find('img.pointerArrowSml');
 		var p = $(this).position();
 		$(arrow).stop(true, true).animate({
 		top:p.top
 		}, 200, function(){});
 	}
-
 	function hoverOut(){
 			
 		if($(this).hasClass('selected')){}else{
@@ -214,7 +293,7 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
 			if($(arrow).is(':animated')){}else{
 				$(arrow).delay('500').animate({
 				top:p.top
-				}, 1150, function(){});
+				}, 650, function(){});
 			}
 		}
 		
@@ -223,13 +302,15 @@ var mediaKit = {/*** Retrieves xml feed, runs template manager, attach onclick a
      over: hoverItem,
      timeout: 0, 
      out: hoverOut
-		
 };
 
-$("nav.nav-sub li").hoverIntent(config);
-$("nav.nav-sub li").click(function(){
+$("nav.nav-sub ul").find('li').hoverIntent(config);
+	}
+$("nav.nav-sub ul").find('li').click(function(){
 $(this).parent('ul').find('li.selected').removeClass('selected');
 $(this).addClass('selected');
+var zoomid = 'a.ad_'+$(this).attr('id');
+$(zoomid).trigger('click');
 });
 
 
