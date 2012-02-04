b21d035752cdc1718d4193962f3ed9b97e66227d
hzane <nicholas.jay.rios@gmail.com>
2012-02-04 00:58:18 -0600

    some copy edit/adds, updated inventory, pricing, fixed benefits bug

--- js/helpers.js
+++ js/helpers.js
@@ -123,3 +123,24 @@ var subDomain = (url.match(new RegExp(/\./g))) ? url : 'dma';
 
 return(subDomain);
 }
+
+
+ /******************* GET URL PARAMS ***********/
+
+function checkParams(parameter) {
+  var loc = location.search.substring(1, location.search.length);
+  var param_value = false;
+
+  var params = loc.split("&");
+  for (i=0; i<params.length;i++) {
+      param_name = params[i].substring(0,params[i].indexOf('='));
+      if (param_name == parameter) {
+          param_value = params[i].substring(params[i].indexOf('=')+1)
+      }
+  }
+  if (param_value) {
+      return param_value;
+  } else {
+      return false;
+  }
+}
